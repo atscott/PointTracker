bf271de08e3d4d9e2b99ba7e6754a35950c50e9d
Andrew Moore <amoore@dhcp-155-92-72-193.nebula.msoe.edu>
2013-04-16 16:07:36 -0500

    Added initial login functionality. (Unfinished)

--- PointTracker/AppDelegate.m
+++ PointTracker/AppDelegate.m
@@ -15,7 +15,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+{    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     UIViewController *viewController1 = [[FirstViewController alloc] initWithNibName:@"FirstViewController" bundle:nil];
@@ -24,6 +24,15 @@
     self.tabBarController.viewControllers = @[viewController1, viewController2];
     self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
+    
+    [Parse setApplicationId:@"nudxR7T6WonMVuXtp7R7Hao2B0VURfTHqa8gPWyk"
+                  clientKey:@"eG3RuQZQiiu8spXM1uBdlIloPE7EZY1udg8WD9aL"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    [testObject setObject:@"bar" forKey:@"foo"];
+    [testObject save];
+    
     return YES;
 }
 
